18284a3b9e96f65f3f8ea5fe6a0186377c9ad390
Bumsoo Kim <bumsookim00@gmail.com>
2024-04-17 16:44:23 +0900

    Add run_FADING.sh

---
+++ run_FADING.sh
@@ -0,0 +1,7 @@
+python age_editing.py \
+--image_path input_sample/00000.png \
+--age_init 3 \
+--gender male \
+--save_aged_dir output_samples \
+--specialized_path finetune_double_prompt_150_random \
+--target_ages 10 20 40 60 80
